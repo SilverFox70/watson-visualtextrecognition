3230e6dc1b0d39f27d4673bc25fe9436761ff0f3
Bill Brinkert <william.brinkert@akqa.com>
2018-01-29 14:50:09 -0800

    feat: add try-catch block; move credentials out of scope to be passed in:

--- VisualTextRecognition.js
+++ VisualTextRecognition.js
@@ -1,40 +1,44 @@
 'use strict';
-require('dotenv').config();
+
 const watson = require('watson-developer-cloud');
 const fs = require('fs');
 
-console.log(`Using api_key: ${process.env.VISUAL_API_KEY}`);
-const visual_recognition = watson.visual_recognition({
-  api_key: process.env.VISUAL_API_KEY,
-  version: 'v3',
-  version_date: '2016-05-20'
-});
-
 class VisualTextRecognition {
-  constructor() {
+  constructor(credentials) {
+    this.visual_recognition = watson.visual_recognition(credentials);
     this.data = {};
   }
 
   findTextIn(file_name){
-    // For production, this should throw an error if no file name is given
-    return new Promise((resolve, reject) => {
-      var params = {
-        images_file: fs.createReadStream(file_name) || fs.createReadStream('imgs/IMG_1089.PNG'),
-        parameters: {}
-      };
+    try {
+      // Fail out as quickly as possible if we don't have a filename
+      if (typeof file_name === 'undefined' || file_name.length === 0) {
+        throw new ReferenceError('A file must be specified to perform this operation.', 'VisualTextRecognition.js', '12');
+      }
 
-      console.log(`[VisualTextRecognition.findTextIn] Sending ${file_name} for analysis @ ${Date.now()}`)
-      visual_recognition.recognize_text(params, function(err, response){
+      return new Promise((resolve, reject) => {
+        var params = {
+          images_file: fs.createReadStream(file_name) || fs.createReadStream('imgs/IMG_1089.PNG'),
+          parameters: {}
+        };
         
-        if (err){ 
-          console.log(`[VisualTextRecoginition.findTextIn] failed @ ${Date.now()}`);
-          reject(err)
-        } else {
-          console.log(`[VisualTextRecoginition.findTextIn] succeeded @ ${Date.now()}`);
-          resolve(response);
-        }
-      }); 
-    });
+        console.log(`[VisualTextRecognition.findTextIn] Sending ${file_name} for analysis @ ${Date.now()}`)
+        this.visual_recognition.recognize_text(params, function(err, response){
+          
+          if (err){ 
+            console.log(`[VisualTextRecoginition.findTextIn] failed @ ${Date.now()}`);
+            reject(err)
+          } else {
+            console.log(`[VisualTextRecoginition.findTextIn] succeeded @ ${Date.now()}`);
+            resolve(response);
+          }
+        }); 
+      });
+
+    } catch(err) {
+      console.log(err);
+      return Promise.reject(err);
+    }
   }
 }
 
